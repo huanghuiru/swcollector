eef3adc87db4367de2ed98126e9e874588b6df70
huanghuiru <31645412+huanghuiru@users.noreply.github.com>
2017-09-27 15:36:05 +0800

    Update custmetric.go

--- funcs/custmetric.go
+++ funcs/custmetric.go
@@ -89,7 +89,7 @@ func custMetrics(ip string, metric *g.MetricConfig, ch chan CustM) {
 	var custmmetric CustmMetric
 	var custmmetrics []CustmMetric
 	switchinfo,_ := hhrmodel.GetInfo()
-	community := hhrmodel.GetPassword(switchinfo,ip)
+	community,_ := hhrmodel.GetPassword(switchinfo,ip)
 	value, err := GetCustMetric(ip, community, metric.Oid, g.Config().Switch.SnmpTimeout, g.Config().Switch.SnmpRetry)
 	if err != nil {
 		log.Println(ip, metric.Oid, err)
