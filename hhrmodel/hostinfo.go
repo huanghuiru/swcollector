06677f37213640a67d3d2506175161cf2a685b40
huanghuiru <31645412+huanghuiru@users.noreply.github.com>
2017-09-27 09:31:32 +0800

    Update hostinfo.go

--- hhrmodel/hostinfo.go
+++ hhrmodel/hostinfo.go
@@ -16,6 +16,7 @@ package hhrmodel
 
 import (
 	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
@@ -30,12 +31,15 @@ func GetIp() (ips []string, err error) {
 	portal, err := gorm.Open("mysql", "root:root@tcp(10.112.95.1:3306)/swcollector")
 	if err != nil {
 		err = fmt.Errorf("connect to swcollector: %s", err.Error())
+		portal.Close()
+		log.Println(err)
 		return
 	}
 	portal.SingularTable(true)
 	portal.AutoMigrate(&Hostinfo{})
 	var hostinfo Hostinfo
 	dt := portal.First(&hostinfo)
+	portal.Close()
 	if dt.Error != nil {
 		err = dt.Error
 		return
