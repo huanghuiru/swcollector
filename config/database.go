7497f8b4c4e8cf9cd9b9920347d66daa76b08ed0
huanghuiru <31645412+huanghuiru@users.noreply.github.com>
2017-09-27 17:12:40 +0800

    Add files via upload

---
+++ config/database.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+"database/sql"
+"fmt"
+
+_ "github.com/go-sql-driver/mysql"
+"github.com/jinzhu/gorm"
+"github.com/spf13/viper"
+)
+
+type DBPool struct {
+	Switchboard    *gorm.DB
+}
+
+var (
+	dbp DBPool
+)
+
+func Con() DBPool {
+	return dbp
+}
+
+func InitDB() (err error) {
+	portal, err := gorm.Open("mysql", "root:root@tcp(10.112.95.1:3306)/gnet")
+	if err != nil {
+		err = fmt.Errorf("connect to swcollector: %s", err.Error())
+		log.Println(err)
+		return
+	}
+	portal.SingularTable(true)
+
+	dbp.Switchboard = portal
+
+	return
+}
+
+func CloseDB() (err error) {
+	err = dbp.Switchboard.Close()
+	if err != nil {
+		return
+	}
+	return
+}
